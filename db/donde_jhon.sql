76518140b89c8a892a8fa45c94e4414fd2184982
ZenSoft17 <jhojanestebanlopezrios1234@gmail.com>
2024-09-17 19:50:05 -0500

    database

--- db/donde_jhon.sql
+++ db/donde_jhon.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 17, 2024 at 05:24 PM
+-- Host: localhost
+-- Generation Time: Sep 18, 2024 at 02:40 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cart` (
   `car_id` int(11) NOT NULL,
-  `use_id` int(11) DEFAULT NULL,
-  `pro_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `pro_id` int(11) DEFAULT NULL,
+  `use_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -41,16 +41,11 @@ CREATE TABLE `cart` (
 
 CREATE TABLE `orders` (
   `ord_id` int(11) NOT NULL,
-  `ord_fullname` varchar(255) DEFAULT NULL,
-  `ord_state` varchar(255) DEFAULT NULL,
-  `ord_city` varchar(255) DEFAULT NULL,
-  `ord_district` varchar(255) DEFAULT NULL,
-  `ord_streettype` varchar(255) DEFAULT NULL,
-  `ord_streetnumber` varchar(255) DEFAULT NULL,
-  `ord_housetype` varchar(255) DEFAULT NULL,
-  `ord_contact` varchar(255) DEFAULT NULL,
-  `ord_comments` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `pro_id` int(11) DEFAULT NULL,
+  `use_id` int(11) DEFAULT NULL,
+  `uor_id` int(11) DEFAULT NULL,
+  `ord_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -61,7 +56,7 @@ CREATE TABLE `orders` (
 CREATE TABLE `products` (
   `pro_id` int(11) NOT NULL,
   `pro_name` varchar(200) NOT NULL,
-  `pro_price` int(255) NOT NULL,
+  `pro_price` int(11) NOT NULL,
   `pro_description` varchar(2000) NOT NULL,
   `pro_total` int(255) NOT NULL,
   `pro_image_1` longblob NOT NULL,
@@ -78,7 +73,7 @@ CREATE TABLE `products` (
 CREATE TABLE `role` (
   `rol_id` int(11) NOT NULL,
   `rol_role` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `role`
@@ -86,7 +81,7 @@ CREATE TABLE `role` (
 
 INSERT INTO `role` (`rol_id`, `rol_role`) VALUES
 (1, 'user'),
-(2, 'admin');
+(2, 'admin\r\n');
 
 -- --------------------------------------------------------
 
@@ -101,6 +96,26 @@ CREATE TABLE `users` (
   `use_pass` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_orders`
+--
+
+CREATE TABLE `users_orders` (
+  `uor_id` int(11) NOT NULL,
+  `use_id` int(11) DEFAULT NULL,
+  `our_full_name` varchar(255) DEFAULT NULL,
+  `our_province` varchar(255) DEFAULT NULL,
+  `our_city` varchar(255) DEFAULT NULL,
+  `our_locality` varchar(255) DEFAULT NULL,
+  `our_street_type` varchar(255) DEFAULT NULL,
+  `our_street_number` varchar(255) DEFAULT NULL,
+  `our_house` varchar(255) DEFAULT NULL,
+  `our_contact_number` varchar(20) DEFAULT NULL,
+  `our_additional_references` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -110,14 +125,17 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`car_id`),
-  ADD KEY `use_id` (`use_id`),
-  ADD KEY `pro_id` (`pro_id`);
+  ADD KEY `pro_id` (`pro_id`),
+  ADD KEY `use_id` (`use_id`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`ord_id`);
+  ADD PRIMARY KEY (`ord_id`),
+  ADD KEY `pro_id` (`pro_id`),
+  ADD KEY `use_id` (`use_id`),
+  ADD KEY `uor_id` (`uor_id`);
 
 --
 -- Indexes for table `products`
@@ -137,6 +155,12 @@ ALTER TABLE `role`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`use_id`),
   ADD KEY `rol_id` (`rol_id`);
+
+--
+-- Indexes for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  ADD PRIMARY KEY (`uor_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -161,6 +185,12 @@ ALTER TABLE `role`
   MODIFY `rol_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  MODIFY `uor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -168,8 +198,16 @@ ALTER TABLE `role`
 -- Constraints for table `cart`
 --
 ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`use_id`) REFERENCES `users` (`use_id`),
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`pro_id`) REFERENCES `products` (`pro_id`);
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`pro_id`) REFERENCES `products` (`pro_id`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`use_id`) REFERENCES `users` (`use_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`pro_id`) REFERENCES `products` (`pro_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`use_id`) REFERENCES `users` (`use_id`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`uor_id`) REFERENCES `users_orders` (`uor_id`);
 
 --
 -- Constraints for table `users`
